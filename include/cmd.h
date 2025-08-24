1f7d3852a68b200bfc299dac3537ecf4bbcd9dd5
Roman Kryvko <roman.kr43@gmail.com>
2025-08-24 17:17:01 +0200

    Extract command line control functionality.

---
+++ include/cmd.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string_view>
+#include <string>
+#include <termios.h>
+
+struct cursor_pos {
+    cursor_pos() = default;
+    cursor_pos(int row, int col) : row(row), col(col) {}
+    int row {};
+    int col {};
+};
+
+const char CTRL_A = CTRL('a');
+const char CTRL_E = CTRL('e');
+const char CTRL_U = CTRL('u');
+const char CTRL_K = CTRL('k');
+const char CTRL_H = CTRL('h');
+const char BACKSPACE = 0x7f;
+const char ENTER = 0xd;
+const char ESC = 0x1b;
+
+void disable_raw_mode();
+
+void enable_raw_mode();
+
+cursor_pos query_cursor_pos();
+
+void set_cursor_position(cursor_pos position);
+
+void redraw_line(std::string_view line);
+
+void move_cursor_right();
+
+void move_cursor_left();
+
+std::string sh_read_line();
