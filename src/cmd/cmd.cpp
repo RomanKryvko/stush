873e4b20c6436ba206210d615ab5f4fbb69fe434
Roman Kryvko <roman.kr43@gmail.com>
2025-09-15 00:16:08 +0200

    Implement pipelines.
    Implement pipelines as a 'proof of concept'. Neither builtins nor |& pipes
    are supported as of now.

--- src/cmd/cmd.cpp
+++ src/cmd/cmd.cpp
@@ -1,30 +1,39 @@
 #include "builtins/builtins.h"
 #include "cmd/cmd.h"
 #include "cmd/expansion.h"
+#include <cassert>
+#include <cstddef>
+#include <cstdio>
 #include <cstdlib>
+#include <sched.h>
 #include <stdexcept>
 #include <string_view>
 #include <unistd.h>
 #include <wait.h>
 
-int run_simple_command_impl(args_view args) {
+void run_process(args_view args) {
+    const char* argv[args.size() + 1];
+    for (size_t i = 0; i < args.size(); i++) {
+        argv[i] = args[i].c_str();
+    }
+    argv[args.size()] = nullptr;
+
+    execvp(argv[0], const_cast<char**>(argv));
+    perror("execvp");
+    exit(EXIT_FAILURE);
+}
+
+/* Run a simple command or a shell builtin. Assumes that all expansions
+ * of variables, globs, etc. have already been done*/
+int run_simple_command(args_view args) {
     int builtin_status {exec_builtin(args)};
     if (builtin_status != BUILTIN_NOT_FOUND)
         return builtin_status;
 
-    const char* args_c [args.size() + 1];
-    for (int i = 0; i < args.size(); i++) {
-        args_c[i] = args[i].c_str();
-    }
-    args_c[args.size()] = nullptr;
-
     int status {};
-    pid_t pid {fork()};
+    const pid_t pid {fork()};
     if (!pid) {
-        if (execvp(args[0].c_str(), const_cast<char**>(args_c)) == -1) {
-            perror("execvp");
-        }
-        exit(EXIT_FAILURE);
+        run_process(args);
     } else if (pid == -1) {
         perror("fork");
     } else {
@@ -35,29 +44,129 @@ int run_simple_command_impl(args_view args) {
     return WEXITSTATUS(status);
 }
 
-int run_simple_command(args_view args) {
-    bool has_wildcard {false};
-
-    for (auto& arg : args) {
+/* Perform variable, tilde, glob expansion and strip quotes. */
+args_container prepare_command_args(args_view args) {
+    args_container result {args.begin(), args.end()};
+    for (auto& arg : result) {
         expand_all_variables(arg);
         expand_tilde(arg);
-        if (arg.find_first_of("*?") != std::string::npos)
-            has_wildcard = true;
+    }
+    expand_globs(result);
+    strip_all_quotes(result);
+    return result;
+}
+
+struct pipeline_item {
+    args_container args;
+    std::string_view separator; //TODO: use enum here?
+};
+
+/* Splits the pipeline by | and |& operators and performs expansions on each
+ * of the resulting commands */
+std::vector<pipeline_item> split_pipeline(args_view args) {
+    std::vector<pipeline_item> res {};
+
+    auto prev {args.begin()};
+    for (auto it = args.begin(); it != args.end(); it++) {
+        const std::string_view& s {*it};
+        if (s == sep::PIPE_OUT || s == sep::PIPE_BOTH) {
+            if (prev == it) {
+                throw std::runtime_error("Missing command in pipeline.");
+            }
+            res.push_back({prepare_command_args({prev, it}), s});
+            prev = it + 1;
+        }
     }
 
-    if (has_wildcard) {
-        args_container copy;
-        copy.assign(args.begin(), args.end());
-        expand_globs(copy);
-        strip_all_quotes(copy);
-        return run_simple_command_impl(copy);
+    if (prev != args.end())
+        res.push_back({prepare_command_args({prev, args.end()}), ""});
+
+    return res;
+}
+
+inline void close_pipes(const std::vector<int[2]>& pipes) {
+    for (auto pipe : pipes) {
+        close(pipe[0]);
+        close(pipe[1]);
     }
-    strip_all_quotes(args);
-    return run_simple_command_impl(args);
 }
 
 int run_pipeline(args_view args) {
-    return run_simple_command(args); //FIXME: implement
+    auto pipelines {split_pipeline(args)};
+    assert(!pipelines.empty());
+
+    const size_t n {pipelines.size()};
+    if (n == 1)
+        return run_simple_command(pipelines[0].args);
+
+    std::vector<int[2]> pipes(n - 1);
+    for (auto p : pipes) {
+        if (pipe(p) == -1) {
+            perror("pipe");
+            return EXIT_FAILURE;
+        }
+    }
+
+    std::vector<pid_t> children (n);
+    //NOTE: this is ugly, but it allows us to avoid ifs
+    {
+        const pid_t pid {fork()};
+        if (!pid) {
+            dup2(pipes[0][1], STDOUT_FILENO);
+
+            close_pipes(pipes);
+
+            //TODO: implement running bultins here
+            run_process(pipelines[0].args);
+        } else if (pid < 0) {
+            perror("fork");
+            return EXIT_FAILURE;
+        }
+        children[0] = pid;
+    }
+    for (size_t i = 1; i < n - 1; i++) {
+        const pid_t pid {fork()};
+        if (!pid) {
+            dup2(pipes[i - 1][0], STDIN_FILENO);
+            dup2(pipes[i][1], STDOUT_FILENO);
+
+            close_pipes(pipes);
+
+            run_process(pipelines[i].args);
+        } else if (pid < 0) {
+            perror("fork");
+            return EXIT_FAILURE;
+        }
+        children[i] = pid;
+    }
+    {
+        const auto last {n - 1};
+        const pid_t pid {fork()};
+        if (!pid) {
+            dup2(pipes[last - 1][0], STDIN_FILENO);
+
+            close_pipes(pipes);
+
+            run_process(pipelines[last].args);
+        } else if (pid < 0) {
+            perror("fork");
+            return EXIT_FAILURE;
+        }
+        children[last] = pid;
+    }
+
+    close_pipes(pipes);
+
+    int pl_status {};
+    for (pid_t pid : children) {
+        int status {};
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        pl_status = status;
+    }
+
+    return WEXITSTATUS(pl_status);
 }
 
 struct list_item {
@@ -119,7 +228,7 @@ std::vector<args_view> split_compound_command(args_container& args) {
         }
     }
 
-    if (prev != args.end() - 1)
+    if (prev != args.end())
         res.push_back({prev, args.end()});
 
     return res;
