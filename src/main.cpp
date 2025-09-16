32466fe28b859100af5f8ec34ee29654322846c5
Roman Kryvko <roman.kr43@gmail.com>
2025-09-16 23:51:49 +0200

    Implement running scripts from files and the command line.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include "cmd/cmd.h"
 #include "linereader/linereader.h"
 #include "parser.h"
+#include <bits/getopt_core.h>
 #include <cassert>
 #include <csignal>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
+#include <string_view>
+#include <sys/types.h>
+
+namespace fs = std::filesystem;
 
 int sh_main_loop(int argc, const char** argv) {
     char delimeter {' '};
@@ -21,7 +30,50 @@ int sh_main_loop(int argc, const char** argv) {
     }
 }
 
+int run_command(std::string_view command, char delimeter = ' ') {
+    args_container args {sh_tokenize(command, delimeter)};
+    return run_compound_command(args);
+}
+
 int main(int argc, char** argv) {
+    int opt {};
+    while ((opt = getopt(argc, argv, "c:")) != -1) {
+        switch (opt) {
+            case 'c': {
+                const int status {run_command(optarg)};
+                exit(status);
+                break;
+            }
+            default: {
+                std::cerr << "Usage: stush [option] script-file\n"
+                    "Options:\n\t-c\texecute a command.\n";
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    if (optind < argc) {
+        try {
+            const auto filename {fs::canonical(argv[optind])};
+            if (!fs::is_regular_file(filename) && !fs::is_symlink(filename)) {
+                std::cerr << "Cannot execute " << std::quoted(argv[optind]) <<
+                    ": is not a regular file or a symlink.\n";
+                exit(EXIT_FAILURE);
+            }
+
+            std::ifstream ifs {filename};
+            std::string line {};
+            int status {};
+            while (std::getline(ifs, line)) {
+                if (!line.empty() && line.front() != '#')
+                    run_command(line);
+            }
+            exit(status);
+        } catch (const fs::filesystem_error& err) {
+            std::cerr << err.what() << '\n';
+            exit(EXIT_FAILURE);
+        }
+    }
+
     signal(SIGINT, SIG_IGN);
     sh_main_loop(argc, (const char**) argv);
 }
